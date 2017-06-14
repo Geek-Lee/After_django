005ee3e7441dc2a43edb1c0c1fa1dc9b7643d791
Geek-Lee <2546701377@qq.com>
2017-06-14 21:07:27 +0800

    分页

--- firstapp/views.py
+++ firstapp/views.py
@@ -2,12 +2,21 @@ from django.shortcuts import render, redirect
 from firstapp.models import Article, Comment
 from firstapp.form import CommentForm
 from django.template import Context, Template
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 # Create your views here.
 
 def index(request):
 	article_list = Article.objects.all()
 	context = {}
+	page_robot = Paginator(article_list, 9)
+	page_num = request.GET.get('page')
+	try:
+		article_list = page_robot.page(page_num)
+	except EmptyPage:
+		article_list = page_robot.page(page_robot.num_pages)
+	except PageNotAnInteger:
+		article_list = page_robot.page(1)
 	context['article_list'] = article_list
 	return render(request,'index.html', context)
 
